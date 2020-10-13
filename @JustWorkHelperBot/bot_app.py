406fcc7a59a0c32b40f0d93ba59aef72d04bee6c
4season <junhoheo11@gmail.com>
2020-10-14 01:50:40 +0900

    Update bot script

--- @JustWorkHelperBot/bot_app.py
+++ @JustWorkHelperBot/bot_app.py
@@ -5,6 +5,7 @@ import json
 import urllib.parse as parse
 import urllib.request as req
 
+
 app = Flask(__name__)
 
 
@@ -12,7 +13,9 @@ def baseRequest(command="getMe"):
     baseURL = 'https://api.telegram.org/bot1300255931:AAF21AHhfaVaai74cSU-X9giO9pJg27x_gU/'
     res = req.urlopen(baseURL+command) #getMe 로드변수
     result = json.loads(res.read())['result']
+    print(result)
     return result
+
 
 def sendMessage(chat_id, text):
     query0 = parse.urlencode([('chat_id', chat_id)])
@@ -29,15 +32,33 @@ def hello_world():
     이 웹사이트는 텔레그램봇 'Just do it!' 을 위해 만들어진 웹사이트 입니다.
     """
 
+
 @app.route('/1300255931:AAF21AHhfaVaai74cSU-X9giO9pJg27x_gU', methods=['POST','GET'])
 def telegram():
     data = request.get_json()
     print(data)
     chat_id = data['message']['chat']['id']
     textM = data['message']['text']
-    if textM == '/help':
-        text = "Help Me!"
-    else:
-        text = "Nope!"
+    if textM == '/get_bot_inof':
+        text = """
+        Just Do It! v(0.1.0)
+        귀찮은 작업을 미루지않게 도와주는 봇입니다.
+        """
+    if textM == '/set_work_new':
+        text = """
+        할일의 이름을 말씀해주세요.
+        """
+    if textM == '/get_work_list':
+        text = """
+        할일이 없다니, 나태하군요.
+        """
+    if textM == '/set_work_on':
+        text = """
+        알림을 시작합니다.
+        """
+    if textM == '/set_work_off':
+        text = """
+        알림을 종료합니다.
+        """
     sendMessage(chat_id,text)
     return json.dumps({'success':True})
