708c3f4346ca99f746f90471bbfc5c914a81703e
FourSeason <junhoheo11@gmail.com>
2020-10-13 20:38:15 +0900

    Create bot_app.py

---
+++ @JustWorkHelperBot/bot_app.py
@@ -0,0 +1,46 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask,request
+import json
+import urllib.parse as parse
+import urllib.request as req
+
+app = Flask(__name__)
+
+
+def baseRequest(command="getMe"):
+    baseURL = 'https://api.telegram.org/bot1300255931:AAF21AHhfaVaai74cSU-X9giO9pJg27x_gU/'
+    res = req.urlopen(baseURL+command) #getMe 로드변수
+    result = json.loads(res.read())['result']
+    return result
+
+def sendMessage(chat_id, text):
+    query = parse.urlencode([
+        ('chat_id',chat_id),
+        ('text',text)
+    ])
+    print(query)
+    baseRequest('sendMessage?'+query)
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!\n이 웹사이트는 텔레그램봇 \'Just do it!\' 을 위해 만들어진 웹사이트 입니다.'
+
+@app.route('/1300255931:AAF21AHhfaVaai74cSU-X9giO9pJg27x_gU', methods=['POST','GET'])
+def telegram():
+    data = request.get_json()
+    print(data)
+    chat_id = data['message']['chat']['id']
+    text = data['message']['text']
+    sendMessage(chat_id,text)
+    return json.dumps({'success0':True})
+
+def telegram1():
+    data = request.get_json()
+    print(data+'2번째데이터')
+    chat_id = data['message']['chat']['id']
+    text = data['message']['entities']['type']
+    sendMessage(chat_id,text)
+    return json.dumps({'success1':True})
